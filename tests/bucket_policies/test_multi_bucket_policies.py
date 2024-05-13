7ad75f8321c55394406588e8f9dd2ae83e32fcd9
Sagi Hirshfeld <shirshfe@redhat.com>
2024-05-13 14:27:50 +0300

    Fix and polish test_multi_bucket_policies.py

--- tests/bucket_policies/test_multi_bucket_policies.py
+++ tests/bucket_policies/test_multi_bucket_policies.py
@@ -4,127 +4,206 @@ from tests.bucket_policies.s3_operation_access_tester import S3OperationAccessTe
 
 class TestMultiBucketPolicies:
     def test_multi_statement_policy(self, c_scope_s3client, s3_client_factory):
-        # Setup
+        """
+        Test multi-statement bucket policies:
+
+        1. Setup:
+            1.1 Create a bucket
+            1.2 Put two objects in the bucket
+            1.3 Create a new account
+        2. Apply a multi-statement policy to a bucket:
+            2.1 Allow all accounts all access to a bucket's objects
+            2.2 Deny all access to a specific object
+            2.3 Apply the policy
+        3. Check that the two statements were applied correctly:
+            3.1 Check that the new account can access the allowed object
+            3.2 Check that the new account can't access the denied object
+
+        """
+        # 1. Setup
         bucket = c_scope_s3client.create_bucket()
         denied_obj, allowed_obj = c_scope_s3client.put_random_objects(bucket, 2)
+        new_acc_client = s3_client_factory()
 
+        # 2. Apply a multi-statement policy
+        # 2.1 Allow all accounts all access to a bucket's objects
         bpb = BucketPolicyBuilder()
-        # 1. Apply a multi-statement policy
-        # Allow all accounts all access to bucket's objects
-        bpb.add_allow_statement().add_action("*").add_principal("*").add_resource(
-            f"{bucket}/*"
+        bpb = (
+            bpb.add_allow_statement()
+            .add_action("*")
+            .add_principal("*")
+            .add_resource(f"{bucket}/*")
         )
-        # Deny all access specifically to the test object
-        bpb.add_deny_statement().add_action("*").add_principal("*").add_resource(
-            denied_obj
+        # 2.2 Deny all access to a specific object
+        bpb = (
+            bpb.add_deny_statement()
+            .add_action("*")
+            .add_principal("*")
+            .add_resource(f"{bucket}/{denied_obj}")
         )
-
-        # Apply the policy
         policy = bpb.build()
-        response = c_scope_s3client.put_bucket_policy(bucket, policy)
+
+        # 2.3 Apply the policy
+        response = c_scope_s3client.put_bucket_policy(bucket, str(policy))
         assert (
             response["Code"] == 200
         ), f"put_bucket_policy failed with code {response['Code']}"
 
-        # 2. Check that the two statements are applied correctly
-        # 2.1 Check that a new account can access the allowed object
-        new_acc_client = s3_client_factory()
-        response = new_acc_client.get_object(bucket, allowed_obj)
-        assert (
-            response["Code"] == 200
-        ), "New account was denied access to the allowed object"
-
-        # 2.2 Check that the original account can't access the denied object
-        response = c_scope_s3client.get_object(bucket, denied_obj)
-        assert (
-            response["Code"] == "AccessDenied"
-        ), "Original account was allowed access to the denied object"
-
-    def test_multi_action_statement_policy(self, c_scope_s3client):
-        bucket = c_scope_s3client.create_bucket()
+        # 3. Check that the two statements are applied correctly
+        # 3.1 Check that a new account can access the allowed object
         access_tester = S3OperationAccessTester(
             admin_client=c_scope_s3client,
         )
+        assert access_tester.check_client_access_to_bucket_op(
+            new_acc_client, bucket, "GetObject", obj_key=allowed_obj
+        ), "Access was denied to the allowed object"
+        # 3.2 Check that the original account can't access the denied object
+        assert not access_tester.check_client_access_to_bucket_op(
+            new_acc_client, bucket, "GetObject", obj_key=denied_obj
+        ), "Access was allowed to the denied object"
 
-        tested_ops = ["GetObject", "PutObject", "DenyObject"]
+    def test_multi_operation_statement_policy(
+        self, c_scope_s3client, s3_client_factory
+    ):
+        """
+        Test multi-operation statement policies:
 
-        # 1. Apply a multi-action statement policy
-        bpb = BucketPolicyBuilder()
-        bpb.add_deny_statement().add_principal("*").add_resource(f"{bucket}/*")
+        1. Setup:
+            1.1 Create a bucket
+            1.2 Create a new account
+        2. Apply a multi-operation statement policy to a bucket
+            2.1 Build a policy that allows multiple operations to all accounts
+            2.2 Apply the policy
+        3. Check that the new account can perform the allowed operations
 
+        """
+        # 1. Setup
+        bucket = c_scope_s3client.create_bucket()
+        new_acc_client = s3_client_factory()
+
+        # 2. Apply a multi-action statement policy
+        # 2.1 Build a policy that allows multiple operations to all accounts
+        tested_ops = ["GetObject", "PutObject", "DeleteObject"]
+        bpb = (
+            BucketPolicyBuilder()
+            .add_allow_statement()
+            .add_principal("*")
+            .add_resource(f"{bucket}/*")
+        )
         for op in tested_ops:
             bpb.add_action(op)
-
         policy = bpb.build()
-        response = c_scope_s3client.put_bucket_policy(bucket, policy)
+
+        # 2.2 Apply the policy
+        response = c_scope_s3client.put_bucket_policy(bucket, str(policy))
         assert (
             response["Code"] == 200
         ), f"put_bucket_policy failed with code {response['Code']}"
 
-        for op in tested_ops:
-            assert not access_tester.check_client_access_to_bucket_op(
-                c_scope_s3client, bucket, op
-            ), f"{op} was allowed for the account when it shouldn't have been"
-
-    def test_multi_resource_statement_policy(self, c_scope_s3client):
-        bucket = c_scope_s3client.create_bucket()
+        # 3. Check that the new account can perform the allowed operations
         access_tester = S3OperationAccessTester(
             admin_client=c_scope_s3client,
         )
+        for op in tested_ops:
+            assert access_tester.check_client_access_to_bucket_op(
+                new_acc_client, bucket, op
+            ), f"{op} was denied for the account when it shouldn't have been"
 
-        tested_objs = c_scope_s3client.put_random_objects(bucket, 2)
+    def test_multi_resource_statement_policy(self, c_scope_s3client, s3_client_factory):
+        """
+        Test multi-resource statement policies:
+
+        1. Setup:
+            1.1 Create a bucket
+            1.2 Put multiple objects in the bucket
+            1.3 Create a new account
+        2. Apply a multi-resource statement policy to a bucket
+            2.1 Build a policy that allows access to multiple objects to all accounts
+            2.2 Apply the policy
+        3. Check that access is allowed to all objects
+
+        """
+
+        # 1. Setup
+        new_acc_client = s3_client_factory()
+        bucket = c_scope_s3client.create_bucket()
+        tested_objs = c_scope_s3client.put_random_objects(bucket, 5)
 
         # 1. Apply a multi-resource statement policy
-        bpb = BucketPolicyBuilder()
-        bpb.add_deny_statement().add_principal("*").add_action("*")
-
+        # 1.1 Build a policy that allows access to multiple objects to all accounts
+        bpb = (
+            BucketPolicyBuilder()
+            .add_allow_statement()
+            .add_principal("*")
+            .add_action("*")
+        )
         for obj in tested_objs:
-            bpb.add_resource(obj)
-
+            bpb.add_resource(f"{bucket}/{obj}")
         policy = bpb.build()
-        response = c_scope_s3client.put_bucket_policy(bucket, policy)
+
+        # 2.2 Apply the policy
+        response = c_scope_s3client.put_bucket_policy(bucket, str(policy))
         assert (
             response["Code"] == 200
         ), f"put_bucket_policy failed with code {response['Code']}"
 
-        # 2. Check that access is denied to all objects
+        # 3. Check that access is denied to all objects
+        access_tester = S3OperationAccessTester(
+            admin_client=c_scope_s3client,
+        )
         for obj in tested_objs:
-            assert not access_tester.check_client_access_to_bucket_op(
-                c_scope_s3client, bucket, "GetObject", obj_key=obj
-            ), f"Access was allowed to {obj} when it shouldn't have been"
+            assert access_tester.check_client_access_to_bucket_op(
+                new_acc_client, bucket, "GetObject", obj_key=obj
+            ), f"Access was denied to {obj} when it shouldn't have been"
 
     def test_multi_principal_statement_policy(
         self, c_scope_s3client, account_manager, s3_client_factory
     ):
+        """
+        Test multi-principal statement policies:
+
+        1. Setup:
+            1.1 Create a bucket
+            1.2 Create multiple accounts
+        2. Apply a multi-principal statement policy to a bucket:
+            2.1 Build a policy that allows multiple principals access to a bucket
+            2.2 Apply the policy
+        3. Check that all principals are allowed access
+
+        """
+        # 1. Setup
         bucket = c_scope_s3client.create_bucket()
-        access_tester = S3OperationAccessTester(
-            admin_client=c_scope_s3client,
-        )
+        acc_names, acc_clients = [], []
+        for _ in range(3):
+            acc_name, access_key, secret_key = account_manager.create()
+            acc_names.append(acc_name)
+            acc_clients.append(
+                s3_client_factory(access_and_secret_keys_tuple=(access_key, secret_key))
+            )
 
-        acc_a_name, access_key, secret_key = account_manager.create()
-        acc_a_client = s3_client_factory(
-            access_and_secret_keys_tuple=(access_key, secret_key)
+        # 2. Apply a multi-principal statement policy
+        # 2.1 Build a policy that allows multiple principals access to a bucket
+        bpb = (
+            BucketPolicyBuilder()
+            .add_allow_statement()
+            .add_action("*")
+            .add_resource(f"{bucket}/*")
         )
-        acc_b_name, access_key, secret_key = account_manager.create()
-        acc_b_client = s3_client_factory(
-            access_and_secret_keys_tuple=(access_key, secret_key)
-        )
-
-        # 1. Apply a multi-principal statement policy
-        bpb = BucketPolicyBuilder()
-        bpb.add_deny_statement().add_action("*").add_resource(f"{bucket}/*")
-        bpb.add_principal(acc_a_name).add_principal(acc_b_name)
-
+        for acc_name in acc_names:
+            bpb.add_principal(acc_name)
         policy = bpb.build()
-        response = c_scope_s3client.put_bucket_policy(bucket, policy)
+
+        # 2.2 Apply the policy
+        response = c_scope_s3client.put_bucket_policy(bucket, str(policy))
         assert (
             response["Code"] == 200
         ), f"put_bucket_policy failed with code {response['Code']}"
 
-        # 2. Check that the two principals are denied access
-        acc_names = [acc_a_name, acc_b_name]
-        acc_clients = [acc_a_client, acc_b_client]
+        # 3. Check that all principals are allowed access
+        access_tester = S3OperationAccessTester(
+            admin_client=c_scope_s3client,
+        )
         for acc, s3_client in zip(acc_names, acc_clients):
-            assert not access_tester.check_client_access_to_bucket_op(
-                s3_client, bucket, acc, "GetObject"
-            ), f"Access was allowed for account {acc} when it shouldn't have been"
+            assert access_tester.check_client_access_to_bucket_op(
+                s3_client, bucket, "GetObject"
+            ), f"Access was denied for account {acc} when it shouldn't have been"
