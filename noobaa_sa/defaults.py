d8ca95162b481b2cda9d4db7585fdeb41eca0ef8
Uday Kurundwade <Uday.Shamrao.Kurundwade@ibm.com>
2024-09-04 17:11:17 +0530

    Removes support of testing health flag with default values
    Signed-off-by: Uday Kurundwade <Uday.Shamrao.Kurundwade@ibm.com>

--- noobaa_sa/defaults.py
+++ noobaa_sa/defaults.py
@@ -2,4 +2,4 @@ import os
 
 NOOBAA_SA_SRC = "/usr/local/noobaa-core"
 MANAGE_NSFS = os.path.join(NOOBAA_SA_SRC, "src/deploy/noobaa-cli")
-HEALTH = os.path.join(NOOBAA_SA_SRC, "src/cmd/health")
+HEALTH = os.path.join(NOOBAA_SA_SRC, "src/deploy/noobaa-cli diagnose health")
