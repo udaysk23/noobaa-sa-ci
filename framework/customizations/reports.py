ac1f7a905dc4db11e6ae993466e53c5796a06e2c
oviner <oviner@redhat.com>
2024-09-25 14:19:16 +0300

    fix code
    Signed-off-by: oviner <oviner@redhat.com>

--- framework/customizations/reports.py
+++ framework/customizations/reports.py
@@ -224,6 +224,7 @@ def email_reports(session):
     soup = BeautifulSoup(html_data, "html.parser")
 
     parse_html_for_email(soup)
+    add_squad_analysis_to_email(session, soup)
     move_summary_to_top(soup)
     add_time_report_to_email(session, soup)
     part1 = MIMEText(soup, "html")
@@ -336,3 +337,168 @@ def decompose_html_attributes(soup, attributes):
         tg = soup.find_all(attrs={"class": attribute})
         for each in tg:
             each.decompose()
+
+
+def add_squad_analysis_to_email(session, soup):
+    """
+    Add squad analysis to the html test results used in email reporting
+
+    Args:
+        session (obj): Pytest session object
+        soup (obj): BeautifulSoup object of HTML Report data
+
+    """
+    failed = {}
+    skipped = {}
+    # sort out failed and skipped test cases to failed and skipped dicts
+    for result in session.results.values():
+        if result.failed or result.skipped:
+            squad_marks = [
+                key[:-6].capitalize() for key in result.keywords if "_squad" in key
+            ]
+            if squad_marks:
+                for squad in squad_marks:
+                    if result.failed:
+                        if squad not in failed:
+                            failed[squad] = []
+                        failed[squad].append(result.nodeid)
+
+                    if result.skipped:
+                        if squad not in skipped:
+                            skipped[squad] = []
+                        try:
+                            skipped_message = result.longrepr[2][8:]
+                        except TypeError:
+                            skipped_message = "--unknown--"
+                        skipped[squad].append((result.nodeid, skipped_message))
+
+            else:
+                # unassigned
+                if result.failed:
+                    if "UNASSIGNED" not in failed:
+                        failed["UNASSIGNED"] = []
+                    failed["UNASSIGNED"].append(result.nodeid)
+                if result.skipped:
+                    if "UNASSIGNED" not in skipped:
+                        skipped["UNASSIGNED"] = []
+                    try:
+                        skipped_message = result.longrepr[2][8:]
+                    except TypeError:
+                        skipped_message = "--unknown--"
+                    skipped["UNASSIGNED"].append((result.nodeid, skipped_message))
+
+    # no failed or skipped tests - exit the function
+    if not failed and not skipped:
+        return
+
+    # add CSS for the Squad Analysis report
+    style = soup.find("style")
+    # use colors for squad names from squad names
+    SQUADS = {
+        "Aqua": ["/lvmo/"],
+        "Brown": ["/z_cluster/", "/nfs_feature/"],
+        "Green": ["/encryption/", "/pv_services/", "/storageclass/"],
+        "Blue": ["/monitoring/"],
+        "Red": ["/mcg/", "/rgw/"],
+        "Purple": ["/ecosystem/"],
+        "Magenta": ["/workloads/", "/flowtest/", "/lifecycle/", "/kcs/", "/system_test/"],
+        "Grey": ["/performance/"],
+        "Orange": ["/scale/"],
+        "Black": ["/ui/"],
+        "Yellow": ["/managed-service/"],
+        "Turquoise": ["/disaster-recovery/"],
+    }
+    style = soup.new_tag("style")
+    style.string = "\n".join(
+        [
+            f"h4.squad-{color.lower()} {{\n    color: {color.lower()};\n}}"
+            for color in SQUADS
+        ]
+    )
+    # few additional styles
+    style.string += """
+    .squad-analysis {
+        color: black;
+        font-family: monospace;
+        background-color: #eee;
+        padding: 5px;
+        margin-top: 10px;
+    }
+    .squad-analysis h2 {
+        margin: 0px;
+    }
+    .squad-analysis h3 {
+        margin: 0px;
+        margin-top: 10px;
+    }
+    .squad-analysis h4 {
+        margin: 0px;
+    }
+    .squad-analysis ul {
+        margin: 0px;
+    }
+    .squad-analysis ul li em {
+        margin-left: 1em;
+    }
+    .squad-unassigned {
+        background-color: #FFBA88;
+    }
+    h4.squad-yellow {
+        color: black;
+        background-color: yellow;
+        display: inline;
+    }
+    """
+    # prepare place for the Squad Analysis in the email
+    squad_analysis_div = soup.new_tag("div")
+    squad_analysis_div["class"] = "squad-analysis"
+    main_header = soup.find("h1")
+    main_header.insert_after(squad_analysis_div)
+    failed_h2_tag = soup.new_tag("h2")
+    failed_h2_tag.string = "Squad Analysis - please analyze:"
+    squad_analysis_div.append(failed_h2_tag)
+    squad = "red"
+    if failed:
+        # print failed testcases peer squad
+        failed_div_tag = soup.new_tag("div")
+        squad_analysis_div.append(failed_div_tag)
+        failed_h3_tag = soup.new_tag("h3")
+        failed_h3_tag.string = "Failures:"
+        failed_div_tag.append(failed_h3_tag)
+
+        failed_h4_tag = soup.new_tag("h4")
+        failed_h4_tag.string = f"{squad} squad"
+        failed_h4_tag["class"] = f"squad-{squad.lower()}"
+        failed_div_tag.append(failed_h4_tag)
+        failed_ul_tag = soup.new_tag("ul")
+        failed_ul_tag["class"] = f"squad-{squad.lower()}"
+        failed_div_tag.append(failed_ul_tag)
+        failed_li_tag = soup.new_tag("li")
+        failed_li_tag.string = "oded1"
+        failed_ul_tag.append(failed_li_tag)
+    if skipped:
+        # print skipped testcases with reason peer squad
+        skips_div_tag = soup.new_tag("div")
+        squad_analysis_div.append(skips_div_tag)
+        skips_h3_tag = soup.new_tag("h3")
+        skips_h3_tag.string = "Skips:"
+        skips_div_tag.append(skips_h3_tag)
+        skip_reason_h4_tag = soup.new_tag("h4")
+        skip_reason_h4_tag.string = config.RUN.get("display_skipped_msg_in_email")
+        skips_div_tag.append(skip_reason_h4_tag)
+        skips_h4_tag = soup.new_tag("h4")
+        skips_h4_tag.string = f"{squad} squad"
+        skips_h4_tag["class"] = f"squad-{squad.lower()}"
+        skips_div_tag.append(skips_h4_tag)
+        skips_ul_tag = soup.new_tag("ul")
+        skips_ul_tag["class"] = f"squad-{squad.lower()}"
+        skips_div_tag.append(skips_ul_tag)
+        skips_li_tag = soup.new_tag("li")
+        skips_test_span_tag = soup.new_tag("span")
+        skips_test_span_tag.string = "oded1"
+        skips_li_tag.append(skips_test_span_tag)
+        skips_li_tag.append(soup.new_tag("br"))
+        skips_reason_em_tag = soup.new_tag("em")
+        skips_reason_em_tag.string = f"Reason: oded"
+        skips_li_tag.append(skips_reason_em_tag)
+        skips_ul_tag.append(skips_li_tag)
